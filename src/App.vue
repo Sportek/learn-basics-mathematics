21771019b5293485f01e69476e2f3c010707e507
= <gablandry31@gmail.com>
2023-11-20 18:51:26 -0500

    Mise en place

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import ContentComponent from './components/ContentComponent.vue'
+import FooterComponent from './components/FooterComponent.vue'
+import HeaderComponent from './components/HeaderComponent.vue'
+</script>
+
+<template>
+  <div class="app">
+    <HeaderComponent />
+
+    <ContentComponent />
+
+    <FooterComponent />
+  </div>
+</template>
+
+<style scoped>
+.app {
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+</style>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import HeartIcon from './icons/HeartIcon.vue'
+</script>
+
+<template>
+  <div class="footer">Fait avec <HeartIcon /> par Gabriel Landry</div>
+</template>
+
+<style scoped>
+.footer {
+  width: 100%;
+  background-color: var(--color-darker-background);
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  padding: 0.5rem;
+}
+
+HeartIcon {
+  display: none;
+}
+</style>
